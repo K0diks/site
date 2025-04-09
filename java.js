2fd1ed9d4c484fad0bbae077434c83dc22aaa8a4
Kodiks <kodiksyt@gmail.com>
2025-04-09 15:12:58 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,11 +1,21 @@
-    document.addEventListener('DOMContentLoaded', () => {
-        let count = 0;
+document.addEventListener('DOMContentLoaded', () => {
+            let count = 0;
 
-        const counterDisplay = document.getElementById('text-of-click1');
-        const clickButton = document.getElementById('button-of-click1');
+            const counterDisplay = document.getElementById('text-of-click1');
+            const clickButton = document.getElementById('button-of-click1');
 
-        clickButton.addEventListener('click', () => {
-            count++;
-            counterDisplay.textContent = count + " клики";
+            clickButton.addEventListener('click', () => {
+                count = count + 100;
+                counterDisplay.textContent = `${count} ${getClickText(count)}`;
+            });
+
+            function getClickText(count) {
+                if (count % 10 === 1 && count % 100 !== 11) {
+                    return 'клик';
+                } else if ((count % 10 >= 2 && count % 10 <= 4) && (count % 100 < 12 || count % 100 > 14)) {
+                    return 'клика';
+                } else {
+                    return 'кликов';
+                }
+            }
         });
-    });
